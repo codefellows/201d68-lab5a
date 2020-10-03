3fa06d64925675a6b7114b32f1f7e43f0569591e
TINA MYERS <myers.tina515@gmail.com>
2020-10-02 17:01:57 -0700

    Answered question five

--- app.js
+++ app.js
@@ -109,11 +109,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  var firstMultiply = multiply(multArr[0], multArr[1]);
+  var productMultiply = multiply(firstMultiply[0], multArr[2]);
+  var stringQuestionFive = 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + productMultiply[0] + '.';
+  return[productMultiply[0], stringQuestionFive];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
